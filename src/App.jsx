df667b5a6e595190e9fb9f7bcfce8faaa1cf2dae
Aravind-pasupuleti-2004 <1706301meheraravind@gmail.com>
2026-02-03 21:29:09 +0530

    Import additional blankImage1 asset in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 import blankImage from "./assets/cbit2.jpg";
+import blankImage1 from "./assets/cbit2.jpg";
 import cbitLogo from "./assets/cbit2.jpg";
 import techeonlogo from "./assets/techeon.jpeg";
 import posterlogo from "./assets/poster.jpeg";
